28a1c81ec0d8274a13336e186831924f6e1d5b22
leizheng8686 <leizheng8686@gmail.com>
2016-04-24 00:56:27 -0400

    Update connectDB.java

--- src/connectDB.java
+++ src/connectDB.java
@@ -4,15 +4,42 @@ import java.util.*;
 //connect MySQL
 //define actionMethod to MySQL
 public class connectDB {
-	
+	String host = "localhost";
+	private Connection conn;
+	private Statement stmt;
+	private ResultSet rs;
+		
 	//connect to local MySQL database
-	public void connection(){
-		
-	}
-	
-	//close connection
-	public void closeconn(){
-		
-	}
-
+	public void connect(){
+			try
+			{//load JDBC driver, make Connection and Statement
+				Class.forName("com.mysql.jdbc.Driver");
+				conn=DriverManager.getConnection("jdbc:mysql://"+host+"/test","root","");
+				stmt=conn.createStatement();
+			}
+			catch(SQLException e)
+			{
+				e.printStackTrace();
+			}
+			catch(ClassNotFoundException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		public void closeConn()
+		{
+			try
+			{
+				if(rs!=null)
+					rs.close();
+				if(stmt!=null)
+					stmt.close();
+				if(conn!=null)
+					conn.close();
+			}
+			catch(SQLException e)
+			{
+				e.printStackTrace();
+			}
+		}
 }
